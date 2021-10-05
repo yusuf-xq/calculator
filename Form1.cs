a37cd6f55c62d5ce318ea06ac6a4744488c86b17
yusuf-xq <76647905+yusuf-xq@users.noreply.github.com>
2021-10-05 21:16:24 +0300

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Calculator github.com/yusuf-xq
+namespace Calculator
 {
     public partial class Form1 : Form
     {
